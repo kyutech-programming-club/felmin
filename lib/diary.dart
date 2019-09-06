990f8a1a18af4a10c93344b7db5351c23a8235b0
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-09-06 15:36:45 +0900

    Move textfield under image

--- lib/diary.dart
+++ lib/diary.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'firebase.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class Diary extends StatefulWidget {
   @override
@@ -8,40 +8,19 @@ class Diary extends StatefulWidget {
 }
 
 class _Diary extends State<Diary> {
-  final myController = TextEditingController();
-  String dateText;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           Container(
-            height: 200.0,
+            height: 300.0,
             width: double.infinity,
             padding: EdgeInsets.all(10.0),
             margin: EdgeInsets.all(10.0),
             child: new Display(Collection: 'answers',Field: 'text',),
           ),
-          TextField(
-            controller: myController,
-            decoration: InputDecoration(
-                hintText: '入力してください'
-            ),
-          ),
-      ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-          dateText = DateTime.now().toString();
-          Firestore.instance.collection('answers').add({
-            'text': '${myController.text}',
-            'timestamp': dateText,
-          });
-          myController.clear();
-        }),
-        tooltip: 'tap',
-        child: Icon(Icons.send),
+        ],
       ),
     );
   }
