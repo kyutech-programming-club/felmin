03e949b6f348892fe2a7ece92731e998ad5595ee
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-09-21 17:13:37 +0900

    Delete debug label

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      //debugShowCheckedModeBanner: false, //右上のデバックが消える
       title: 'Felemin',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
